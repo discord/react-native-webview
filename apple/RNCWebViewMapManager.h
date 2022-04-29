5856cbe8317dfc983bf23bd8a79ed3436d667cbe
Donald Chen <donald.chen@discordapp.com>
2022-04-29 12:25:51 -0700

    add back the observer without the EXC_BAD_ACCESS error

---
+++ apple/RNCWebViewMapManager.h
@@ -0,0 +1,13 @@
+//
+//  RNCWebViewMapManager.h
+//  RNCWebView
+//
+//  Created by Donald Chen on 4/29/22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#ifndef RNCWebViewMapManager_h
+#define RNCWebViewMapManager_h
+
+
+#endif /* RNCWebViewMapManager_h */
---
+++ apple/RNCWebViewMapManager.m
@@ -0,0 +1,9 @@
+//
+//  RNCWebViewMapManager.m
+//  RNCWebView
+//
+//  Created by Donald Chen on 4/29/22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
