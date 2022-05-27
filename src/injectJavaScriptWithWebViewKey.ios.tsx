4eb0155ce377def22da2e0bf426296862d2adc04
Donald Chen <donald.chen@discordapp.com>
2022-05-27 15:39:06 -0700

    fix method name

--- src/injectJavaScriptWithWebViewKey.ios.tsx
+++ src/injectJavaScriptWithWebViewKey.ios.tsx
@@ -1,5 +1,5 @@
 import { NativeModules } from 'react-native';
 
-export default function injectJavaScript(webViewKey: string, script: string) {
+export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string) {
   NativeModules.RNCWebViewManager.injectJavaScriptWithWebViewKey(webViewKey, script);
 }
