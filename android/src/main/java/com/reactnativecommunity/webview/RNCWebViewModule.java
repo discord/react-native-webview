56fd883dccd1e635c60b5a48f99b7aaf51dcad8a
Neal Manaktola <neal.manaktola@discordapp.com>
2022-08-31 11:15:17 -0400

    fix boolean

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewModule.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewModule.java
@@ -173,7 +173,7 @@ public class RNCWebViewModule extends ReactContextBaseJavaModule implements Acti
       }
 
       // Perform webview cleanup
-      if (webView.webViewKey != null || webView.keepWebViewInstanceAfterUnmount) {
+      if (webView.webViewKey != null && webView.keepWebViewInstanceAfterUnmount) {
         ((ThemedReactContext) webView.getContext()).removeLifecycleEventListener(webView);
         webView.cleanupCallbacksAndDestroy();
       }
