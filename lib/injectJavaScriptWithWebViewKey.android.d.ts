a1465abf153e58461c6916f669b2005e03e374dd
Donald Chen <donald.chen@discordapp.com>
2022-09-08 14:55:38 -0700

    yarn build

--- lib/injectJavaScriptWithWebViewKey.android.d.ts
+++ lib/injectJavaScriptWithWebViewKey.android.d.ts
@@ -1,2 +1,2 @@
-export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): void;
+export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): Promise<string | null>;
 //# sourceMappingURL=injectJavaScriptWithWebViewKey.android.d.ts.map
--- lib/injectJavaScriptWithWebViewKey.ios.d.ts
+++ lib/injectJavaScriptWithWebViewKey.ios.d.ts
@@ -1,2 +1,2 @@
-export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): void;
+export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): Promise<string | null>;
 //# sourceMappingURL=injectJavaScriptWithWebViewKey.ios.d.ts.map
