c712de673efefd301fef6c0e31f7e1745acbd82d
Donald Chen <donald.chen@discordapp.com>
2022-09-08 15:07:50 -0700

    make it async

--- src/injectJavaScriptWithWebViewKey.ios.tsx
+++ src/injectJavaScriptWithWebViewKey.ios.tsx
@@ -1,5 +1,5 @@
 import { NativeModules } from 'react-native';
 
-export default function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): Promise<string | null> {
+export default async function injectJavaScriptWithWebViewKey(webViewKey: string, script: string): Promise<string | null> {
   return NativeModules.RNCWebViewManager.injectJavaScriptWithWebViewKey(webViewKey, script);
 }
--- src/injectJavaScriptWithWebViewKey.tsx
+++ src/injectJavaScriptWithWebViewKey.tsx
@@ -1,4 +1,4 @@
-export default function injectJavaScriptWithWebViewKey(_webViewKey: string, _script: string, ): Promise<string | null> {
+export default async function injectJavaScriptWithWebViewKey(_webViewKey: string, _script: string, ): Promise<string | null> {
   // no-op. This should get overwritten by platform-specific implementations
   return Promise.resolve(null);
 }
