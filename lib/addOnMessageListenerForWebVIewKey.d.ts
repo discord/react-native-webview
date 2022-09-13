98449e081b8483b33f618e2bc77cbea0748080b1
Neal Manaktola <neal.manaktola@discordapp.com>
2022-09-13 10:11:28 -0400

    additional cleanup

--- lib/addOnMessageListenerForWebVIewKey.d.ts
+++
@@ -1,3 +0,0 @@
-import { EmitterSubscription } from "react-native";
-export default function addOnMessageListenerForWebViewKey(webViewKey: string, listener: (event: any) => void): EmitterSubscription;
-//# sourceMappingURL=addOnMessageListenerForWebViewKey.d.ts.map
